bbbf8d8b83aa7d4e21cab592984a2e858f249a7e
Krin12 <130473834+Krin12@users.noreply.github.com>
2023-12-11 01:10:54 +0900

    add Ex8

--- Ex8/hellogtk.c
+++ Ex8/hellogtk.c
@@ -1,30 +1,27 @@
 #include <gtk/gtk.h>
 
-static void destroy(GtkWidget *widget, gpointer data )
-{
-	gtk_widget_destroy (data);
+void button_clicked(GtkWidget *widget, gpointer data) {
+    g_print("Hello, World!\n");
+    gtk_main_quit();
 }
 
-static void activate(GtkApplication *app, gpointer data )
-{
-	GtkWidget *window, *button;
-	window = gtk_application_window_new (app);
-	gtk_window_set_title (GTK_WINDOW (window), "Window");
-	gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
-	button = gtk_button_new_with_label ("Hello World");
-	g_signal_connect (button, "clicked", G_CALLBACK (destroy), NULL);
-	gtk_container_add (GTK_CONTAINER (window), button);
-	gtk_widget_show_all (window);
-}
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
 
-int main(int argc, char *argv[])
-{
-	GtkApplication *app;
-	int status;
-	app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-	g_signal_connect (app, "activate", G_CALLBACK(activate), NULL);
-	status = g_application_run (G_APPLICATION (app), argc, argv);
-	g_object_unref (app);
-	gtk_main ();
-	return 0;
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Hello, World!");
+
+    GtkWidget *button = gtk_button_new_with_label("Hello World!");
+    
+    g_signal_connect(button, "clicked", G_CALLBACK(button_clicked), NULL);
+
+    gtk_container_add(GTK_CONTAINER(window), button);
+
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_widget_show_all(window);
+
+    gtk_main();
+
+    return 0;
 }
